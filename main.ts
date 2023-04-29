ba1116b29797b288888cc6eb3f1a97a501dd5311
FelixVsAlt <123268405+FelixVsAlt@users.noreply.github.com>
2023-04-29 21:33:31 +0200

    55

--- main.ts
+++ main.ts
@@ -1,67 +1,60 @@
 basic.forever(function () {
-    basic.showLeds(`
-        . # # # .
-        # # . # #
-        # . # . #
-        . # # # .
-        # . . . #
-        `)
-    for (let index = 0; index < 120; index++) {
-        basic.pause(5000)
-        basic.pause(5000)
-    }
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(349, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(349, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(277, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(294, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(311, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(330, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(349, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(370, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(392, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(415, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(440, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(466, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(523, music.beat(BeatFraction.Half))
+    basic.pause(11)
+    music.playTone(554, music.beat(BeatFraction.Half))
+    basic.pause(11)
     for (let index = 0; index < 6; index++) {
-        basic.pause(5000)
-        basic.pause(5000)
+        music.playTone(587, music.beat(BeatFraction.Half))
+        basic.pause(11)
+        music.playTone(587, music.beat(BeatFraction.Half))
+        basic.pause(11)
+        music.playTone(587, music.beat(BeatFraction.Half))
+        basic.pause(11)
     }
-    for (let index = 0; index < 4; index++) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        music.playTone(294, music.beat(BeatFraction.Whole))
-        music.playTone(330, music.beat(BeatFraction.Whole))
-        music.playTone(349, music.beat(BeatFraction.Whole))
-        music.playTone(392, music.beat(BeatFraction.Whole))
-        music.playTone(349, music.beat(BeatFraction.Whole))
-        music.playTone(330, music.beat(BeatFraction.Whole))
-        music.playTone(294, music.beat(BeatFraction.Whole))
-    }
-    for (let index = 0; index < 6; index++) {
-        basic.pause(5000)
-        basic.pause(5000)
-    }
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(349, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(392, music.beat(BeatFraction.Breve))
-    music.playTone(349, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    basic.showLeds(`
-        . # # # .
-        # # . # #
-        # . # . #
-        . # # # .
-        # . . . #
-        `)
-    break;
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
 })
